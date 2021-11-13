8f61b98b8fb86566376a8587f4fcf5b0cfd7cff4
Cristian Badic <cristi_ano07@yahoo.com>
2021-11-13 15:01:39 +0200

    new project

---
+++ stefan-leonard/src/App.js
@@ -0,0 +1,45 @@
+import wolves from "./components/assets/video/holanda.mp4";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <video
+          autoPlay
+          loop
+          muted
+          style={{
+            position: "absolute",
+            width: "100%",
+            left: "50%",
+            top: "50%",
+            height: "100%",
+            objectFit: "cover",
+            transform: "translate(-50%, -50%)",
+            zIndex: "-1",
+          }}
+        >
+          <source src={wolves} type="video/mp4" />
+        </video>
+      </div>
+
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
